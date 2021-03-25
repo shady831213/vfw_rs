7295a5d6187583920607326b0363ce476828b4dc
shady831213 <shady831213@126.com>
2021-03-25 02:19:05 -0700

    init

--- platform/terminus_cosim/build.sh
+++ platform/terminus_cosim/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
 TESTNAME=$1
+#you may modify the $LIBCLANG_PATH and $RISCV_TOOLCHAIN_PREFIX
 TESTNAME=$TESTNAME LIBCLANG_PATH=/usr/lib/llvm-12/lib RISCV_TOOLCHAIN_PREFIX=riscv32-unknown-elf- cargo build -p terminus_cosim_tests -Zunstable-options --release --out-dir target/$TESTNAME --bin $TESTNAME
 TESTNAME=$TESTNAME LIBCLANG_PATH=/usr/lib/llvm-12/lib RISCV_TOOLCHAIN_PREFIX=riscv32-unknown-elf- cargo objdump -p terminus_cosim_tests --release --bin $TESTNAME -- -D >|target/$TESTNAME/$TESTNAME.dump

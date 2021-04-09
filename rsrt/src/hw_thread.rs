14134ed70ed52691e6e5eccaf370500242dffc4c
shady831213 <shady831213@126.com>
2021-04-09 03:05:51 -0700

    fix fake join

--- rsrt/src/hw_thread.rs
+++ rsrt/src/hw_thread.rs
@@ -22,8 +22,31 @@ pub struct Task {
     pub args: [usize; 8],
 }
 
-static CPUS: [Mutex<Option<Task>>; MAX_CORES] =
-    [const { Mutex::new(Option::<Task>::None) }; MAX_CORES];
+#[derive(Copy, Clone)]
+#[repr(C)]
+struct CpuState {
+    task: Option<Task>,
+    cur_cnt: u16,
+    finish_cnt: u16,
+}
+impl CpuState {
+    const fn empty() -> CpuState {
+        CpuState {
+            task: None,
+            cur_cnt: 0,
+            finish_cnt: 0,
+        }
+    }
+    fn is_done(&self, id: u16) -> bool {
+        if (id & 0x8000) != (self.finish_cnt & 0x8000) {
+            self.finish_cnt < id
+        } else {
+            self.finish_cnt > id
+        }
+    }
+}
+
+static CPUS: [Mutex<CpuState>; MAX_CORES] = [const { Mutex::new(CpuState::empty()) }; MAX_CORES];
 
 fn schedule(hartid: usize) {
     send_others_ipis(hartid, 0xffffffff)
@@ -37,17 +60,21 @@ pub fn idle() {
     let hartid = hartid();
     loop {
         {
-            let cur_task = CPUS[hartid].lock();
-            if let Some(task) = cur_task.as_ref() {
+            let cpu = CPUS[hartid].lock();
+            if let Some(task) = cpu.task.as_ref() {
                 let t = *task;
                 //should drop the lock first then send ipi
-                drop(cur_task);
+                drop(cpu);
                 (run_task().unwrap())(&t);
-                *CPUS[hartid].lock() = None;
+                {
+                    let mut cpu = CPUS[hartid].lock();
+                    cpu.finish_cnt = cpu.finish_cnt.wrapping_add(1);
+                    cpu.task = None;
+                }
                 schedule(hartid);
             } else {
                 //should drop the lock before sleep
-                drop(cur_task);
+                drop(cpu);
                 //Allow cpu enter interrupt when goto sleep, if there's soft int, handler should ignore it. Then after go back from handler,
                 //wfi can exit.
                 //Besides, idle/fork/join should never called in interrupt context! So we don't need save/restore flag.
@@ -212,8 +239,8 @@ fn __try_fork_on(
     if args_len > 8 {
         return Err("number of args is larger than max args length 8!");
     }
-    let mut cur_task = CPUS[target_id].lock();
-    if cur_task.as_ref().is_some() {
+    let mut cpu = CPUS[target_id].lock();
+    if cpu.task.as_ref().is_some() {
         Err("busy!")
     } else {
         let mut task = Task {
@@ -225,24 +252,28 @@ fn __try_fork_on(
                 *((args as usize + i * core::mem::size_of::<usize>() as usize) as *const usize)
             };
         }
-        *cur_task = Some(task);
+        let task_id = (target_id << 16) | cpu.cur_cnt as usize;
+        cpu.cur_cnt = cpu.cur_cnt.wrapping_add(1);
+        cpu.task = Some(task);
         schedule_on(target_id);
-        Ok(target_id)
+        Ok(task_id)
     }
 }
 
 #[no_mangle]
 pub extern "C" fn join(task_id: usize) {
     let hartid = hartid();
+    let target_id = task_id >> 16;
+    let seq_id = task_id as u16;
     loop {
-        let cur_task = CPUS[task_id].lock();
-        if cur_task.as_ref().is_some() {
-            drop(cur_task);
+        let cpu = CPUS[target_id].lock();
+        if cpu.is_done(seq_id) {
+            break;
+        } else {
+            drop(cpu);
             // let flag = save_flag();
             wait_ipi(hartid);
             // restore_flag(flag);
-        } else {
-            break;
         }
     }
 }

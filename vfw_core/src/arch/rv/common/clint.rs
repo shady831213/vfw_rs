c8b0f0c44d6c46e6cc772d3861efbeda6aa2d119
shady831213 <shady831213@126.com>
2023-09-13 13:17:26 +0800

    clint support 32 access for rv64

--- vfw_core/src/arch/rv/common/clint.rs
+++ vfw_core/src/arch/rv/common/clint.rs
@@ -1,24 +1,47 @@
 #![allow(dead_code)]
-use vfw_primitives::{io_read64, io_write32, io_write64};
+use vfw_primitives::{io_read32, io_read64, io_write32, io_write64};
 
 const MTIME_OFF: usize = 0xbff8;
 const TIMER_OFF: usize = 0x4000;
 
 pub struct Clint {
     base: usize,
+    access_64: bool,
 }
 
 impl Clint {
-    pub const fn new(base: usize) -> Clint {
-        Clint { base: base }
+    pub const fn new(base: usize, access_64: bool) -> Clint {
+        Clint { base, access_64 }
     }
 
     pub fn get_mtime(&self) -> u64 {
-        io_read64!(self.base + MTIME_OFF)
+        if self.access_64 {
+            io_read64!(self.base + MTIME_OFF)
+        } else {
+            let mut hi = io_read32!(self.base + MTIME_OFF + 4);
+            let mut lo = io_read32!(self.base + MTIME_OFF);
+            loop {
+                let new_hi = io_read32!(self.base + MTIME_OFF + 4);
+                if new_hi == hi {
+                    break ((new_hi as u64) << 32) | (lo as u64);
+                } else {
+                    hi = new_hi;
+                    lo = io_read32!(self.base + MTIME_OFF);
+                }
+            }
+        }
     }
 
     pub fn set_timer(&self, hart_id: usize, instant: u64) {
-        io_write64!(self.base + TIMER_OFF + (hart_id << 3), instant)
+        if self.access_64 {
+            io_write64!(self.base + TIMER_OFF + (hart_id << 3), instant)
+        } else {
+            io_write32!(self.base + TIMER_OFF + (hart_id << 3), instant as u32);
+            io_write32!(
+                self.base + TIMER_OFF + (hart_id << 3) + 4,
+                (instant >> 32) as u32
+            );
+        }
     }
 
     pub fn send_soft(&self, hart_id: usize) {
